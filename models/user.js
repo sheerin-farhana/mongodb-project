2688499c4775609c8ff96213af5f2b82ffdecd11
sheerin-farhana <sheerinfarhana25@gmail.com>
2024-03-01 20:04:11 +0530

    users table created

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const getDb = require("../util/database").getDb;
+const mongodb = require("mongodb");
+
+const ObjectId = mongodb.ObjectId;
+
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .then((user) => {
+        console.log(user);
+        return user;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+module.exports = User;

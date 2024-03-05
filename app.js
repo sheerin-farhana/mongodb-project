a8bb2de85e922837dce38d077ab59580a93811ca
sheerin-farhana <sheerinfarhana25@gmail.com>
2024-03-05 16:42:45 +0530

    CRUD operations done using mongoose

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { config } = require("dotenv");
+config();
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
 
-const User = require("./models/user");
+// const User = require("./models/user");
 
 const app = express();
 
@@ -19,20 +21,26 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("65e1df61e340003d7b86c898")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("65e1df61e340003d7b86c898")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(process.env.MONGOURL)
+  .then((result) => {
+    app.listen(3000);
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
